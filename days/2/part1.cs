fd0c78e0e6bb5cf3285fe63377830d3d4b7bae15
Geekid812 <geekid812@gmail.com>
2020-12-09 20:30:11 +0100

    Day 2 - C#

---
+++ days/2/part1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Day2
+{
+    class Part1
+    {
+        static void Main(string[] args)
+        {
+            string content = System.IO.File.ReadAllText("input.txt");
+            string[] passwords = content.Split("\n");
+            int count = 0;
+            
+            foreach (string line in passwords) {
+                string[] parts = line.Split(" ");
+                int seperatorIndex = parts[0].IndexOf("-");
+
+                int minOccurences = int.Parse(parts[0].Substring(0, seperatorIndex));
+                int maxOccurences = int.Parse(parts[0].Substring(seperatorIndex + 1, parts[0].Length - seperatorIndex - 1));
+                char characterToCount = parts[1][0];
+                string password = parts[2];
+
+                int numOccurences = password.Count(c => c == characterToCount);
+                if (minOccurences <= numOccurences && numOccurences <= maxOccurences) {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Part 1: There are {0} valid passwords.", count);
+            Part2.Start(args);
+        }
+    }
+}
---
+++ days/2/part2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Day2
+{
+    class Part2
+    {
+        public static void Start(string[] args)
+        {
+            string content = System.IO.File.ReadAllText("input.txt");
+            string[] passwords = content.Split("\n");
+            int count = 0;
+            
+            foreach (string line in passwords) {
+                string[] parts = line.Split(" ");
+                int seperatorIndex = parts[0].IndexOf("-");
+
+                int firstIndex = int.Parse(parts[0].Substring(0, seperatorIndex)) - 1;
+                int secondIndex = int.Parse(parts[0].Substring(seperatorIndex + 1, parts[0].Length - seperatorIndex - 1)) - 1;
+                char characterToCheck = parts[1][0];
+                string password = parts[2];
+
+                if (characterToCheck == password[firstIndex] ^ characterToCheck == password[secondIndex]) {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Part 2: There are {0} valid passwords.", count);
+        }
+    }
+}
